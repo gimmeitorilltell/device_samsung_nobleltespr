df228d9e659cd5d324405d782a990b0d041c618f
gimme <gimmeitorilltell@gmail.com>
2018-03-30 03:08:24 -0400

    Move all to common repo
    Change-Id: I893ee50e26e9645bbb79a64217d3eebcf9486701

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,133 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
 
+# inherit from noblelte-common
+include device/samsung/noblelte-common/BoardConfigCommon.mk
+
+# Assert
 TARGET_OTA_ASSERT_DEVICE := noblelte,nobleltespr
 
-NOBLELTE_PATH := device/samsung/nobleltespr
-
 # Include path
-TARGET_SPECIFIC_HEADER_PATH := $(NOBLELTE_PATH)/include
-
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-ENABLE_CPUSETS := true
-
-# Binder
-TARGET_USES_64_BIT_BINDER := true
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(NOBLELTE_PATH)/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := $(NOBLELTE_PATH)/bluetooth/libbt_vndcfg.txt
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := universal7420
-TARGET_NO_BOOTLOADER := true
-
-# Charger
-BOARD_BATTERY_DEVICE_NAME := battery
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
-
-# Graphics
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
-
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_sec
-TARGET_UNIFIED_DEVICE := true
-
-#TARGET_RELEASETOOLS_EXTENSIONS := $(NOBLELTE_PATH)
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/nobleltespr/include
 
 # Kernel
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x10008000 --ramdisk_offset 0x11000000 --tags_offset 0x10000100 --dt $(NOBLELTE_PATH)/dtb.img
-#TARGET_KERNEL_CONFIG := cm_nobleltespr_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos7420
-TARGET_USES_UNCOMPRESSED_KERNEL := true
+TARGET_KERNEL_CONFIG := candy_nobleltespr_defconfig
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
-# NFC
-BOARD_NFC_HAL_SUFFIX := universal7420
-
-# Partitions
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_BOOTIMAGE_PARTITION_SIZE := 29360128
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 35651584
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3656552448
-#BOARD_USERDATAIMAGE_PARTITION_SIZE := 59183980544 #64GB
-
-# Recovery
-TARGET_RECOVERY_FSTAB := $(NOBLELTE_PATH)/rootdir/etc/fstab.samsungexynos7420
-
-# Platform
-TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := blobs
-
-# Radio
-BOARD_VENDOR := samsung
-BOARD_RIL_CLASS := ../../../$(NOBLELTE_PATH)/ril
-
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_HAS_DOWNLOAD_MODE := true
-
-# Renderscript
-BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
-BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a57
-
-# Sensors
-TARGET_NO_SENSOR_PERMISSION_CHECK := true
-
-# WEBGL
-ENABLE_WEBGL := true
-
-# Wifi
-BOARD_HAVE_SAMSUNG_WIFI          := true
-BOARD_WLAN_DEVICE                := bcmdhd
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
-WIFI_DRIVER_NVRAM_PATH           := "/etc/wifi/nvram_net.txt_b0"
-WIFI_DRIVER_FW_PATH_STA          := "/etc/wifi/bcmdhd_sta.bin_b0"
-WIFI_DRIVER_FW_PATH_AP           := "/etc/wifi/bcmdhd_apsta.bin_b1"
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-
-# inherit from the proprietary version
--include vendor/samsung/nobleltespr/BoardConfigVendor.mk
+# Properties
+PRODUCT_COPY_FILES += \
+    device/samsung/nobleltespr/system.prop:system/vendor/build.prop
